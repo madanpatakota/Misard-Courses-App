1f847b571e216250cdf317acff915d05b03c8c44
Madan Patakota <madan.patakota@gmail.com>
2024-04-04 18:02:39 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AngularComponent } from './angular/angular.component';
+import { HtmlComponent } from './html/html.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { TypescriptComponent } from './typescript/typescript.component';
+import { BootstrapComponent } from './bootstrap/bootstrap.component';
+import { CsharpComponent } from './csharp/csharp.component';
+import { AspnetcoreComponent } from './aspnetcore/aspnetcore.component';
+import { AwsComponent } from './aws/aws.component';
+import { AzureComponent } from './azure/azure.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { InterviewsComponent } from './interviews/interviews.component';
+import { CourseupdatesComponent } from './courseupdates/courseupdates.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponent,
+    HtmlComponent,
+    JavascriptComponent,
+    TypescriptComponent,
+    BootstrapComponent,
+    CsharpComponent,
+    AspnetcoreComponent,
+    AwsComponent,
+    AzureComponent,
+    ArticlesComponent,
+    InterviewsComponent,
+    CourseupdatesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/csharp/csharp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csharp',
+  templateUrl: './csharp.component.html',
+  styleUrls: ['./csharp.component.css']
+})
+export class CsharpComponent {
+
+}
